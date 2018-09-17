30631dc43ca57c43e393c7d058700726c007564d
123 <>
2018-09-18 01:04:37 +0300

    fon

--- core/src/ru/geekbrains/stargame/StarGame.java
+++ core/src/ru/geekbrains/stargame/StarGame.java
@@ -5,26 +5,28 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	TextureRegion region;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("fon.jpg");
+		region = new TextureRegion(img,0,0,2048,1100);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		Gdx.gl.glClear(GL20.GL_IMPLEMENTATION_COLOR_READ_TYPE);
 		batch.begin();
-		batch.draw(img,100,100);
-		batch.setColor(1,1,1,0.8f);
-		batch.draw(img, 0, 0);
-		batch.
+		batch.draw(img,0,0);
+		batch.setColor(1,1,1,1);
+		batch.draw(region, 0, 0);
 		batch.end();
 	}
 	
