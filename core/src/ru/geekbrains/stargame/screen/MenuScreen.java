ff5eebbf059ce301c528cbc7b41f0ae102373fc2
123 <>
2018-09-24 00:39:25 +0300

    starship

--- core/src/ru/geekbrains/stargame/screen/MenuScreen.java
+++ core/src/ru/geekbrains/stargame/screen/MenuScreen.java
@@ -1,29 +1,34 @@
 package ru.geekbrains.stargame.screen;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-
 import com.badlogic.gdx.math.Vector2;
+
+import ru.geekbrains.stargame.base.ActionListener;
+import ru.geekbrains.stargame.sprites.Background;
 import ru.geekbrains.stargame.base.Base2DScreen;
+import ru.geekbrains.stargame.math.Rect;
+import ru.geekbrains.stargame.sprites.ButtonExit;
+import ru.geekbrains.stargame.sprites.ButtonPlay;
+import ru.geekbrains.stargame.sprites.Star;
 
-public class MenuScreen extends Base2DScreen {
 
-    SpriteBatch batch;
-    Texture img;
-    Texture img2;
-    TextureRegion region;
-    Vector2 pos;
-    Vector2 pos2;
+public class MenuScreen extends Base2DScreen implements ActionListener {
 
-    Vector2 v;
-    Vector2 v2;
-    Vector2 v3;
-    Vector2 xxx;
+    private static final int STAR_COUNT = 256;
+
+    Background background;
+    Texture bg;
+    TextureAtlas atlas;
+
+    ButtonExit buttonExit;
+    ButtonPlay buttonPlay;
+
+    Star[] star;
 
     public MenuScreen(Game game) {
         super(game);
@@ -32,106 +37,82 @@ public class MenuScreen extends Base2DScreen {
     @Override
     public void show() {
         super.show();
-        batch = new SpriteBatch();
-        img = new Texture("fon.jpg");
-        img2=new Texture("starship.jpg");
-        region = new TextureRegion(img2,0,0,145,198);
-        pos =new Vector2(1f,0f);
-        pos2=new Vector2(0f,0f);
-        v=new Vector2(1f,0f);
-        v2=new Vector2(0f,0f);
-        v3=new Vector2(0f,0f);
-//        xxx=new Vector2(20,10);
-//        v=new Vector2(xxx);
-
+        bg = new Texture("fon.jpg");
+        background = new Background(new TextureRegion(bg));
+        atlas = new TextureAtlas("textures/menuAtlas.tpack");
+        buttonExit = new ButtonExit(atlas, this);
+        buttonPlay = new ButtonPlay(atlas, this);
+        star = new Star[STAR_COUNT];
+        for (int i = 0; i < star.length; i++) {
+            star[i] = new Star(atlas);
+        }
     }
+
 
     @Override
     public void render(float delta) {
         super.render(delta);
+        update(delta);
+        draw();
+    }
+
+    public void update(float delta) {
+        for (int i = 0; i < star.length; i++) {
+            star[i].update(delta);
+        }
+    }
+
+    public void draw() {
         Gdx.gl.glClearColor(1, 0.4f, 0.6f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(img,0,0);
-//        batch.setColor(1,1,1,1);
-        batch.draw(region, pos.x, pos.y);
+        background.draw(batch);
+        for (int i = 0; i < star.length; i++) {
+            star[i].draw(batch);
+        }
+        buttonExit.draw(batch);
+        buttonPlay.draw(batch);
         batch.end();
-//        System.out.println("pos=" + pos + " pos2=" + pos2);
-//        if (pos==pos2) {
-//            pos.add(v2);
-//            System.out.println("STOPPPPPPPPPPPPPPPPPPPPPPPPPPPPP");
-//        }
-//        v3=v.cpy().add(v2);
-//            System.out.println("STOPPPPPPPPPPPPPPPPPPPPPPPPPPPPP");
-//            pos.add(v3);
-    if (pos.x==pos2.x) {
-        v.x=0;
-        v.y=0;
-    }else pos.add(v);
+    }
 
 
-
-//    if (pos.y==pos2.y) {
-////        v.x=0;
-//        v.y=0;
-//    }else pos.add(v);
-
+    @Override
+    protected void resize(Rect worldBounds) {
+        background.resize(worldBounds);
+        for (int i = 0; i < star.length; i++) {
+            star[i].resize(worldBounds);
+        }
+        buttonExit.resize(worldBounds);
+        buttonPlay.resize(worldBounds);
     }
 
     @Override
     public void dispose() {
-
-        batch.dispose();
-        img.dispose();
+        bg.dispose();
+        atlas.dispose();
         super.dispose();
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        System.out.println("screenX=" + screenX + " screenY=" + (Gdx.graphics.getHeight()-screenY));
-        int z=0;
-        if (pos.x<=screenX   ) {
-            pos2 = new Vector2(screenX, Gdx.graphics.getHeight()-screenY);
-//            v=new Vector2(z+1,0);
-            z=z+1;
-        }else if(pos.x>screenX    ) {
-            pos2 = new Vector2(screenX, Gdx.graphics.getHeight()-screenY);
-//            v=new Vector2(z-1,0);
-            z=z-1;
-        }
-        int z2=0;
-        if ( pos.y<=Gdx.graphics.getHeight()-screenY  ) {
-            pos2 = new Vector2(screenX, screenY);
-//            v=new Vector2(z,z2+1);
-            z2=z2+1;
-        }else if (  pos.y>Gdx.graphics.getHeight()-screenY   ) {
-            pos2 = new Vector2(screenX, screenY);
-//            v=new Vector2(z,z2-1);
-            z2=z2-1;
-
-        }
-        v=new Vector2(z,z2);
-
-//        if (pos.y<=screenY ) {
-//            pos2 = new Vector2(screenX, screenY);
-//            v=new Vector2(0,1);
-//        }
-//        if(pos.y>screenY) {
-//            pos2 = new Vector2(screenX, screenY);
-//            v=new Vector2(0,-1);
-//        }
-
-//        v.x=screenX;
-//        if (pos==pos2) {
-//            v2 = new Vector2(1f,0f);
-//        }
-        return super.touchDown(screenX, screenY, pointer, button);
+    public boolean touchDown(Vector2 touch, int pointer) {
+        buttonExit.touchDown(touch, pointer);
+        buttonPlay.touchDown(touch, pointer);
+        return super.touchDown(touch, pointer);
     }
 
     @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-//        System.out.println("screenX=" + screenX + " screenY=" + (Gdx.graphics.getHeight()-screenY));
+    public boolean touchUp(Vector2 touch, int pointer) {
+        buttonExit.touchUp(touch, pointer);
+        buttonPlay.touchUp(touch, pointer);
+        return super.touchUp(touch, pointer);
+    }
 
-        return super.mouseMoved(screenX, screenY);
+    @Override
+    public void actionPerformed(Object src) {
+        if (src == buttonExit) {
+            Gdx.app.exit();
+        } else if (src == buttonPlay) {
+            game.setScreen(new GameScreen(game));
+        }
     }
 }
